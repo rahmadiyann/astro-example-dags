311080baf59e78d1b6669c0cc2b1299050718653
Rahmadiyan Muhammad <81454957+rianying@users.noreply.github.com>
2024-04-01 02:38:27 +0700

    Create dontdie.py

---
+++ dags/dontdie.py
@@ -0,0 +1,30 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator, DummyOperator
+from pendulum import datetime
+import requests
+
+#Define the basic parameters of the DAG, like schedule and start_date
+dag = DAG(
+    "dontdie",
+    start_date=datetime(2024, 4, 1),
+    schedule="*/5 * * * *",
+    catchup=True,
+    default_args={"owner": "rian"},
+    tags=["example"],
+)
+
+#Define tasks
+
+def call_dontdie():
+    r = requests.get("http://www.rian.social/dontdie")
+    print(r.text)
+
+start = DummyOperator(task_id="start", dag=dag)
+end = DummyOperator(task_id="end", dag=dag)
+call = PythonOperator(
+    task_id="call_dontdie",
+    python_callable=call_dontdie,
+    dag=dag,
+)
+
+start >> call >> end
