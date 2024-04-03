1b5824a5c4df3af5a15a4de58a40ba627d480b7b
Rahmadiyan Muhammad <81454957+rianying@users.noreply.github.com>
2024-04-03 07:37:14 +0700

    Update dbops.py

--- dags/scripts/dbops.py
+++ dags/scripts/dbops.py
@@ -51,84 +51,84 @@ def fetch_status():
 
 #get the name from table priority_job
 
-def get_priority_job():
-    cur = conn.cursor()
-    cur.execute("SELECT name, start_time FROM priority_job ORDER BY id")
-    names_tuples = cur.fetchall()
-    cur.close()
-    jobs = []
-    for name_tuple in names_tuples:
-        name, start_time = name_tuple
-        hour, minute = map(int, start_time.split(':'))
-        job_dict = {
-            "name": name,
-            "hour": hour,
-            "minute": minute
-        }
-        jobs.append(job_dict)
-    return jobs
+# def get_priority_job():
+#     cur = conn.cursor()
+#     cur.execute("SELECT name, start_time FROM priority_job ORDER BY id")
+#     names_tuples = cur.fetchall()
+#     cur.close()
+#     jobs = []
+#     for name_tuple in names_tuples:
+#         name, start_time = name_tuple
+#         hour, minute = map(int, start_time.split(':'))
+#         job_dict = {
+#             "name": name,
+#             "hour": hour,
+#             "minute": minute
+#         }
+#         jobs.append(job_dict)
+#     return jobs
 
-names = get_priority_job()
-for name in names:
-    #create a new .py file for each name
-    with open(f'/Users/rian/Documents/GitHub/astro-example-dags/dags/{name["name"]}.py', 'w') as f:
-        file_content = f"""
-import pendulum
-from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
-from airflow.operators.dummy_operator import DummyOperator
-from datetime import datetime, timedelta
-import random
-from scripts.dbops import update_end_time, update_status
-import time
+# names = get_priority_job()
+# for name in names:
+#     #create a new .py file for each name
+#     with open(f'/Users/rian/Documents/GitHub/astro-example-dags/dags/{name["name"]}.py', 'w') as f:
+#         file_content = f"""
+# import pendulum
+# from airflow import DAG
+# from airflow.operators.python_operator import PythonOperator
+# from airflow.operators.dummy_operator import DummyOperator
+# from datetime import datetime, timedelta
+# import random
+# from scripts.dbops import update_end_time, update_status
+# import time
 
-local_tz = pendulum.timezone('Asia/Jakarta')
+# local_tz = pendulum.timezone('Asia/Jakarta')
 
-default_args = {{
-    'owner': 'rian',
-    'depends_on_past': False,
-    'start_date': datetime(2024, 4, 1, tzinfo=local_tz),
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-}}
-dagname = "{name['name']}"
-dag = DAG(
-    dagname,
-    default_args=default_args,
-    description=dagname,
-    schedule_interval='{name['minute']} {name['hour']} * * *',
-    catchup=False,
-    tags=['priority']
-)
-start = PythonOperator(
-    task_id='start',
-    python_callable=update_status,
-    op_kwargs={{"dagname": dagname}},
-    dag=dag,
-)
-done = PythonOperator(
-    task_id='done',
-    python_callable=update_end_time,
-    op_kwargs={{"dagname": dagname}},  # Pass your parameter here as a string
-    dag=dag,
-)
-def run(a,b):
-    #random sleep in minute
-    start = a*60
-    end = b*60
-    sleep = random.randint(start, end)
-    print("I'm going to sleep for " + str(sleep) + " second")
-    time.sleep(sleep)
-    print("I woke up after " + str(sleep) + " second")
+# default_args = {{
+#     'owner': 'rian',
+#     'depends_on_past': False,
+#     'start_date': datetime(2024, 4, 1, tzinfo=local_tz),
+#     'email_on_failure': False,
+#     'email_on_retry': False,
+#     'retries': 1,
+#     'retry_delay': timedelta(minutes=5),
+# }}
+# dagname = "{name['name']}"
+# dag = DAG(
+#     dagname,
+#     default_args=default_args,
+#     description=dagname,
+#     schedule_interval='{name['minute']} {name['hour']} * * *',
+#     catchup=False,
+#     tags=['priority']
+# )
+# start = PythonOperator(
+#     task_id='start',
+#     python_callable=update_status,
+#     op_kwargs={{"dagname": dagname}},
+#     dag=dag,
+# )
+# done = PythonOperator(
+#     task_id='done',
+#     python_callable=update_end_time,
+#     op_kwargs={{"dagname": dagname}},  # Pass your parameter here as a string
+#     dag=dag,
+# )
+# def run(a,b):
+#     #random sleep in minute
+#     start = a*60
+#     end = b*60
+#     sleep = random.randint(start, end)
+#     print("I'm going to sleep for " + str(sleep) + " second")
+#     time.sleep(sleep)
+#     print("I woke up after " + str(sleep) + " second")
 
-execute = PythonOperator(
-    task_id='execute',
-    python_callable=run,
-    op_kwargs={{"a": 1, "b": 5}},
-)
+# execute = PythonOperator(
+#     task_id='execute',
+#     python_callable=run,
+#     op_kwargs={{"a": 1, "b": 5}},
+# )
 
-start >> execute >> done
-"""
-        f.write(file_content)
+# start >> execute >> done
+# """
+#         f.write(file_content)
