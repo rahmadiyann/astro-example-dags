2880b96ffe759295f99370d2bdb484d4b0e7d3e1
Rahmadiyan Muhammad <81454957+rianying@users.noreply.github.com>
2024-04-06 19:39:40 +0700

    Create trackresi.py

---
+++ dags/trackresi.py
@@ -0,0 +1,50 @@
+
+import pendulum
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.dummy_operator import DummyOperator
+from datetime import datetime, timedelta
+import requests
+
+local_tz = pendulum.timezone('Asia/Jakarta')
+
+default_args = {
+    'owner': 'rian',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 4, 1, tzinfo=local_tz),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+dagname = "cbs_loaniq_to_ods"
+dag = DAG(
+    dagname,
+    default_args=default_args,
+    description=dagname,
+    schedule_interval='0 */1 * * *',
+    catchup=False,
+    tags=['priority']
+)
+start = DummyOperator(
+    task_id='start',
+    op_kwargs={"dagname": dagname},
+    dag=dag,
+)
+
+def call():
+    response = requests.get("https://rian.social/api/cekresi/track")
+    print(response.text)
+
+execute = PythonOperator(
+    task_id='execute',
+    python_callable=call,
+    dag=dag,
+)
+
+end = DummyOperator(
+    task_id='end',
+    dag=dag,
+)
+
+start >> execute >> end
