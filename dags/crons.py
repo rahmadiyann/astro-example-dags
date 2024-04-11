4e634719f455127f423eaa7f228c6442c903d7f3
Rahmadiyan Muhammad <81454957+rianying@users.noreply.github.com>
2024-04-11 12:21:47 +0700

    .

--- dags/crons.py
+++ dags/crons.py
@@ -6,23 +6,24 @@ import requests
 
 #Define the basic parameters of the DAG, like schedule and start_date
 dag = DAG(
-    "dontdie",
+    "crons",
     start_date=datetime(2024, 4, 1, 0, 0),
-    schedule="*/10 * * * *",
+    schedule="*/5 * * * *",
     catchup=True,
     default_args={"owner": "rian"},
-    tags=["example"],
+    tags=["priority"],
 )
 
 #Define tasks
 
 sites = {
-    "rian.social": "http://www.rian.social/dontdie",
-    # "rahmadiyan_backend": "https://riansbackend.onrender.com/dontdie"
+    "rian.social": "https://www.rian.social/dontdie",
+    "sw_auto": "https://www.rian.social/swauto/process",
+    "track_resi": "https://www.rian.social/pakeeeet/track"
 }
 
-def call_dontdie(url):
-    r = requests.get("http://www.rian.social/dontdie")
+def call(url):
+    r = requests.get(url)
     print(r.text)
 
 start = DummyOperator(task_id="start", dag=dag)
@@ -30,7 +31,7 @@ end = DummyOperator(task_id="end", dag=dag)
 for site, url in sites.items():
     call = PythonOperator(
         task_id=f"call_{site}",
-        python_callable=call_dontdie,
+        python_callable=call,
         op_kwargs={"url": url},
         dag=dag,
     )
